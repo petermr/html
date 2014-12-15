53a42289eaad739bb60a73f2045457dc3b3f681a
Mark Williamson <mw529@cam.ac.uk>
2014-12-15 15:43:12 +0000

    Fix for issue #2

--- src/test/java/org/xmlcml/html/util/HtmlUnitTest.java
+++ src/test/java/org/xmlcml/html/util/HtmlUnitTest.java
@@ -10,7 +10,6 @@ import org.xmlcml.html.HtmlElement;
 public class HtmlUnitTest {
 
 	@Test
-	@Ignore // fails because of Xerces xml-apis problem - see POM
 	public void testHtmlUnit() throws Exception {
 		URL url = new URL("http://www.biomedcentral.com/1471-2229/14/106");
 		HtmlElement htmlElement = HtmlUtil.readAndCreateElementUsingHtmlUnit(url);
