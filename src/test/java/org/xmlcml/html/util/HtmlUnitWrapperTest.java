307f0e009eed6f51ee005aa5b42b66e37a1c7d81
petermr <none@none>
2015-01-10 10:07:27 +0000

    minor change HtmlUnitWrapperTest

--- src/test/java/org/xmlcml/html/util/HtmlUnitWrapperTest.java
+++ src/test/java/org/xmlcml/html/util/HtmlUnitWrapperTest.java
@@ -69,5 +69,36 @@ public class HtmlUnitWrapperTest {
 		}
 		XMLUtil.debug(htmlElement, new FileOutputStream("target/htmlunitEls.html"), 1);
 	}
+	
+	@Test
+	/** try to use at headless browser on Elsevier - doesn't seem to work.
+	 * 
+	 * @throws Exception
+	 */
+	public void testHtmlUnitElsevierNY() throws Exception {
+		URL url = new URL("	http://www.sciencedirect.com/science/article/pii/S1055790314003923?np=y");
+		HtmlUnitWrapper htmlUnitWrapper = new HtmlUnitWrapper();
+		HtmlElement htmlElement = htmlUnitWrapper.readAndCreateElement(url);
+		Assert.assertNotNull("read element", htmlElement);
+		Nodes nodes = htmlElement.query( 
+				"//*["
+				+ "local-name()='script'"
+//				+ " or local-name()='link'"
+//				+ " or local-name()='object'"
+//				+ " or local-name()='iframe' "
+//				+ " or local-name()='fieldset' "
+//				+ " or local-name()='button' "
+//				+ " or local-name()='style' "
+//				+ " or @class='mobile-hidden' "
+//				+ " or @id='left-article-box' "
+//				+ " or @id='branding' "
+				+ "] "
+				+ "| //comment()"
+				+ "");
+		for (int i = nodes.size()-1; i >= 0; i--) {
+			nodes.get(i).detach();
+		}
+		XMLUtil.debug(htmlElement, new FileOutputStream("target/htmlunitEls.html"), 1);
+	}
 }
 
