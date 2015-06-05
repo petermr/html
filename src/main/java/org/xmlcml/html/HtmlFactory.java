8325981025a56acf26a20403c309179d3e1f6ea1
petermr <none@none>
2015-06-05 21:56:38 +0100

    added config to HtmlFactory

--- src/main/java/org/xmlcml/html/HtmlFactory.java
+++ src/main/java/org/xmlcml/html/HtmlFactory.java
@@ -16,6 +16,7 @@ import nu.xom.Node;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.xmlcml.graphics.svg.SVGConstants;
@@ -33,7 +34,10 @@ import org.xmlcml.xml.XMLUtil;
  */
 public class HtmlFactory {
 
-	private final static Logger LOG = Logger.getLogger(HtmlFactory.class);
+	private static final Logger LOG = Logger.getLogger(HtmlFactory.class);
+	static {
+		LOG.setLevel(Level.DEBUG);
+	}
 
 	private static Map<String, String> DEFAULT_REPLACEMENT_MAP = null;
     static final String[][] ESCAPES_XML = {
@@ -54,12 +58,12 @@ public class HtmlFactory {
         {"\u00A6", "brvbar"}, // broken bar = broken vertical bar
         {"\u00A7", "sect"}, // section sign
         {"\u00A8", "uml"}, // diaeresis = spacing diaeresis
-        {"\u00A9", "copy"}, // © - copyright sign
+        {"\u00A9", "copy"}, // �� - copyright sign
         {"\u00AA", "ordf"}, // feminine ordinal indicator
         {"\u00AB", "laquo"}, // left-pointing double angle quotation mark = left pointing guillemet
         {"\u00AC", "not"}, // not sign
         {"\u00AD", "shy"}, // soft hyphen = discretionary hyphen
-        {"\u00AE", "reg"}, // ® - registered trademark sign
+        {"\u00AE", "reg"}, // �� - registered trademark sign
         {"\u00AF", "macr"}, // macron = spacing macron = overline = APL overbar
         {"\u00B0", "deg"}, // degree sign
         {"\u00B1", "plusmn"}, // plus-minus sign = plus-or-minus sign
@@ -77,70 +81,70 @@ public class HtmlFactory {
         {"\u00BD", "frac12"}, // vulgar fraction one half = fraction one half
         {"\u00BE", "frac34"}, // vulgar fraction three quarters = fraction three quarters
         {"\u00BF", "iquest"}, // inverted question mark = turned question mark
-        {"\u00C0", "Agrave"}, // А - uppercase A, grave accent
-        {"\u00C1", "Aacute"}, // Б - uppercase A, acute accent
-        {"\u00C2", "Acirc"}, // В - uppercase A, circumflex accent
-        {"\u00C3", "Atilde"}, // Г - uppercase A, tilde
-        {"\u00C4", "Auml"}, // Д - uppercase A, umlaut
-        {"\u00C5", "Aring"}, // Е - uppercase A, ring
-        {"\u00C6", "AElig"}, // Ж - uppercase AE
-        {"\u00C7", "Ccedil"}, // З - uppercase C, cedilla
-        {"\u00C8", "Egrave"}, // И - uppercase E, grave accent
-        {"\u00C9", "Eacute"}, // Й - uppercase E, acute accent
-        {"\u00CA", "Ecirc"}, // К - uppercase E, circumflex accent
-        {"\u00CB", "Euml"}, // Л - uppercase E, umlaut
-        {"\u00CC", "Igrave"}, // М - uppercase I, grave accent
-        {"\u00CD", "Iacute"}, // Н - uppercase I, acute accent
-        {"\u00CE", "Icirc"}, // О - uppercase I, circumflex accent
-        {"\u00CF", "Iuml"}, // П - uppercase I, umlaut
-        {"\u00D0", "ETH"}, // Р - uppercase Eth, Icelandic
-        {"\u00D1", "Ntilde"}, // С - uppercase N, tilde
-        {"\u00D2", "Ograve"}, // Т - uppercase O, grave accent
-        {"\u00D3", "Oacute"}, // У - uppercase O, acute accent
-        {"\u00D4", "Ocirc"}, // Ф - uppercase O, circumflex accent
-        {"\u00D5", "Otilde"}, // Х - uppercase O, tilde
-        {"\u00D6", "Ouml"}, // Ц - uppercase O, umlaut
+        {"\u00C0", "Agrave"}, // �� - uppercase A, grave accent
+        {"\u00C1", "Aacute"}, // �� - uppercase A, acute accent
+        {"\u00C2", "Acirc"}, // �� - uppercase A, circumflex accent
+        {"\u00C3", "Atilde"}, // �� - uppercase A, tilde
+        {"\u00C4", "Auml"}, // �� - uppercase A, umlaut
+        {"\u00C5", "Aring"}, // �� - uppercase A, ring
+        {"\u00C6", "AElig"}, // �� - uppercase AE
+        {"\u00C7", "Ccedil"}, // �� - uppercase C, cedilla
+        {"\u00C8", "Egrave"}, // �� - uppercase E, grave accent
+        {"\u00C9", "Eacute"}, // �� - uppercase E, acute accent
+        {"\u00CA", "Ecirc"}, // �� - uppercase E, circumflex accent
+        {"\u00CB", "Euml"}, // �� - uppercase E, umlaut
+        {"\u00CC", "Igrave"}, // �� - uppercase I, grave accent
+        {"\u00CD", "Iacute"}, // �� - uppercase I, acute accent
+        {"\u00CE", "Icirc"}, // �� - uppercase I, circumflex accent
+        {"\u00CF", "Iuml"}, // �� - uppercase I, umlaut
+        {"\u00D0", "ETH"}, // �� - uppercase Eth, Icelandic
+        {"\u00D1", "Ntilde"}, // �� - uppercase N, tilde
+        {"\u00D2", "Ograve"}, // �� - uppercase O, grave accent
+        {"\u00D3", "Oacute"}, // �� - uppercase O, acute accent
+        {"\u00D4", "Ocirc"}, // �� - uppercase O, circumflex accent
+        {"\u00D5", "Otilde"}, // �� - uppercase O, tilde
+        {"\u00D6", "Ouml"}, // �� - uppercase O, umlaut
         {"\u00D7", "times"}, // multiplication sign
-        {"\u00D8", "Oslash"}, // Ш - uppercase O, slash
-        {"\u00D9", "Ugrave"}, // Щ - uppercase U, grave accent
-        {"\u00DA", "Uacute"}, // Ъ - uppercase U, acute accent
-        {"\u00DB", "Ucirc"}, // Ы - uppercase U, circumflex accent
-        {"\u00DC", "Uuml"}, // Ь - uppercase U, umlaut
-        {"\u00DD", "Yacute"}, // Э - uppercase Y, acute accent
-        {"\u00DE", "THORN"}, // Ю - uppercase THORN, Icelandic
-        {"\u00DF", "szlig"}, // Я - lowercase sharps, German
-        {"\u00E0", "agrave"}, // а - lowercase a, grave accent
-        {"\u00E1", "aacute"}, // б - lowercase a, acute accent
-        {"\u00E2", "acirc"}, // в - lowercase a, circumflex accent
-        {"\u00E3", "atilde"}, // г - lowercase a, tilde
-        {"\u00E4", "auml"}, // д - lowercase a, umlaut
-        {"\u00E5", "aring"}, // е - lowercase a, ring
-        {"\u00E6", "aelig"}, // ж - lowercase ae
-        {"\u00E7", "ccedil"}, // з - lowercase c, cedilla
-        {"\u00E8", "egrave"}, // и - lowercase e, grave accent
-        {"\u00E9", "eacute"}, // й - lowercase e, acute accent
-        {"\u00EA", "ecirc"}, // к - lowercase e, circumflex accent
-        {"\u00EB", "euml"}, // л - lowercase e, umlaut
-        {"\u00EC", "igrave"}, // м - lowercase i, grave accent
-        {"\u00ED", "iacute"}, // н - lowercase i, acute accent
-        {"\u00EE", "icirc"}, // о - lowercase i, circumflex accent
-        {"\u00EF", "iuml"}, // п - lowercase i, umlaut
-        {"\u00F0", "eth"}, // р - lowercase eth, Icelandic
-        {"\u00F1", "ntilde"}, // с - lowercase n, tilde
-        {"\u00F2", "ograve"}, // т - lowercase o, grave accent
-        {"\u00F3", "oacute"}, // у - lowercase o, acute accent
-        {"\u00F4", "ocirc"}, // ф - lowercase o, circumflex accent
-        {"\u00F5", "otilde"}, // х - lowercase o, tilde
-        {"\u00F6", "ouml"}, // ц - lowercase o, umlaut
+        {"\u00D8", "Oslash"}, // �� - uppercase O, slash
+        {"\u00D9", "Ugrave"}, // �� - uppercase U, grave accent
+        {"\u00DA", "Uacute"}, // �� - uppercase U, acute accent
+        {"\u00DB", "Ucirc"}, // �� - uppercase U, circumflex accent
+        {"\u00DC", "Uuml"}, // �� - uppercase U, umlaut
+        {"\u00DD", "Yacute"}, // �� - uppercase Y, acute accent
+        {"\u00DE", "THORN"}, // �� - uppercase THORN, Icelandic
+        {"\u00DF", "szlig"}, // �� - lowercase sharps, German
+        {"\u00E0", "agrave"}, // �� - lowercase a, grave accent
+        {"\u00E1", "aacute"}, // �� - lowercase a, acute accent
+        {"\u00E2", "acirc"}, // �� - lowercase a, circumflex accent
+        {"\u00E3", "atilde"}, // �� - lowercase a, tilde
+        {"\u00E4", "auml"}, // �� - lowercase a, umlaut
+        {"\u00E5", "aring"}, // �� - lowercase a, ring
+        {"\u00E6", "aelig"}, // �� - lowercase ae
+        {"\u00E7", "ccedil"}, // �� - lowercase c, cedilla
+        {"\u00E8", "egrave"}, // �� - lowercase e, grave accent
+        {"\u00E9", "eacute"}, // �� - lowercase e, acute accent
+        {"\u00EA", "ecirc"}, // �� - lowercase e, circumflex accent
+        {"\u00EB", "euml"}, // �� - lowercase e, umlaut
+        {"\u00EC", "igrave"}, // �� - lowercase i, grave accent
+        {"\u00ED", "iacute"}, // �� - lowercase i, acute accent
+        {"\u00EE", "icirc"}, // �� - lowercase i, circumflex accent
+        {"\u00EF", "iuml"}, // �� - lowercase i, umlaut
+        {"\u00F0", "eth"}, // �� - lowercase eth, Icelandic
+        {"\u00F1", "ntilde"}, // �� - lowercase n, tilde
+        {"\u00F2", "ograve"}, // �� - lowercase o, grave accent
+        {"\u00F3", "oacute"}, // �� - lowercase o, acute accent
+        {"\u00F4", "ocirc"}, // �� - lowercase o, circumflex accent
+        {"\u00F5", "otilde"}, // �� - lowercase o, tilde
+        {"\u00F6", "ouml"}, // �� - lowercase o, umlaut
         {"\u00F7", "divide"}, // division sign
-        {"\u00F8", "oslash"}, // ш - lowercase o, slash
-        {"\u00F9", "ugrave"}, // щ - lowercase u, grave accent
-        {"\u00FA", "uacute"}, // ъ - lowercase u, acute accent
-        {"\u00FB", "ucirc"}, // ы - lowercase u, circumflex accent
-        {"\u00FC", "uuml"}, // ь - lowercase u, umlaut
-        {"\u00FD", "yacute"}, // э - lowercase y, acute accent
-        {"\u00FE", "thorn"}, // ю - lowercase thorn, Icelandic
-        {"\u00FF", "yuml"}, // я - lowercase y, umlaut
+        {"\u00F8", "oslash"}, // �� - lowercase o, slash
+        {"\u00F9", "ugrave"}, // �� - lowercase u, grave accent
+        {"\u00FA", "uacute"}, // �� - lowercase u, acute accent
+        {"\u00FB", "ucirc"}, // �� - lowercase u, circumflex accent
+        {"\u00FC", "uuml"}, // �� - lowercase u, umlaut
+        {"\u00FD", "yacute"}, // �� - lowercase y, acute accent
+        {"\u00FE", "thorn"}, // �� - lowercase thorn, Icelandic
+        {"\u00FF", "yuml"}, // �� - lowercase y, umlaut
     };
 
 
@@ -163,6 +167,9 @@ public class HtmlFactory {
 	private Map<String, String> replacementMap;
 	private boolean stripDoctype = true;
 	private boolean useJsoup = true;
+	private List<String> contentList;
+	private List<String> noContentList;
+	private List<String> balanceList;
 	private boolean abortOnError = false;
 	private boolean ignoreNamespaces = true;
 	private List<String> tagToDeleteList;
@@ -230,6 +237,31 @@ public class HtmlFactory {
 		replacementMap.put(old, replacement);
 	}
 	
+	public List<String> getContentList() {
+		return contentList;
+	}
+
+	public void setContentList(List<String> removeList) {
+		this.contentList = removeList;
+	}
+
+	
+	public List<String> getNoContentList() {
+		return noContentList;
+	}
+
+	public void setNoContentList(List<String> noContentList) {
+		this.noContentList = noContentList;
+	}
+
+	public List<String> getBalanceList() {
+		return balanceList;
+	}
+
+	public void setBalanceList(List<String> balanceList) {
+		this.balanceList = balanceList;
+	}
+
 	/**
 	 * @return the useJsoup
 	 */
@@ -507,9 +539,63 @@ public class HtmlFactory {
 
 	public HtmlElement parse(InputStream is) throws Exception {
 		String ss = IOUtils.toString(is, "UTF-8");
+		ensureContentLists();
+		for (String remove : contentList) {
+			ss = XMLUtil.removeTags(remove, ss);
+		}
+		for (String remove : noContentList) {
+			ss = removeElementsWithoutContent(remove, ss);
+		}
+		for (String balance : balanceList) {
+			ss = balanceElements(balance, ss);
+		}
+		ss = removeNamespacePrefixes(ss);
 		ss = parseLegacyHtmlToWellFormedXML(ss);
+		ss = tidyTidyingErrors(ss);
 		HtmlElement htmlElement = parseToXHTML(ss);
+//		FileUtils.write(new File("target/debug/"+ss.length()+".html"), htmlElement.toXML());
 		return htmlElement;
+	}
+
+	private void ensureContentLists() {
+		if (contentList == null) {
+			contentList = new ArrayList<String>();
+		}
+		if (noContentList == null) {
+			noContentList = new ArrayList<String>();
+		}
+		if (balanceList == null) {
+			balanceList = new ArrayList<String>();
+		}
+		// TODO Auto-generated method stub
+		
+	}
+
+	private String removeNamespacePrefixes(String ss) {
+		ss = ss.replaceAll("<[a-z]+:", "<");
+		ss = ss.replaceAll("</[a-z]+:", "</");
+		return ss;
+	}
+
+	private String tidyTidyingErrors(String ss) {
+		return ss.replaceAll("\"=\"\"", "");
+	}
+
+	private String balanceElements(String balance, String ss) {
+//		ss = ss.replaceAll("<"+balance+"[^>]*>", "");
+//		ss = ss.replaceAll("</"+balance+"></"+balance+">", "");
+		return ss;
+	}
+
+	private String removeElementsWithContent(String remove, String ss) {
+		ss = ss.replaceAll("<"+remove+"[^>]*>[^<]*</"+remove+">", "");
+		return ss;
+	}
+
+	private String removeElementsWithoutContent(String remove, String ss) {
+		ss = ss.replaceAll("<"+remove+"[^>]*/?>", "");
+		ss = ss.replaceAll("</"+remove+">", "");
+		return ss;
 	}
 
 	private HtmlElement parseToXHTML(String ss) throws IOException {
